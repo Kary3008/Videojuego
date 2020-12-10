dff14f17b9073953c421e2ba199a89e2d2875df0
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2020-12-09 20:01:11 -0600

    Videojuego
    Carpetas con los elementos para la función del videojuego.

---
+++ Assets/scripts/jump.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+    public float fuerzaSalto;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+            {
+          //  anim.SetBool("estaSaltando", true);
+            rb.AddForce(new Vector2(0, fuerzaSalto));
+        }
+    }
+}
